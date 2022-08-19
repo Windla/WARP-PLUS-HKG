265b28a6b1a43dab9778f8ef271fb94980ee85ce
Windla <63698964+Windla@users.noreply.github.com>
2022-08-19 17:06:39 +0800

    Add files via upload

--- warp-HKG.py
+++ warp-HKG.py
@@ -1,4 +1,5 @@
 # coding = uft-8
+from asyncio.windows_events import NULL
 import requests
 import win32serviceutil
 import time
@@ -8,11 +9,17 @@ import os
 from ping3 import ping
 
 
-# Settings
-# If your want to other，please change this.
+#
+# ------------Settings------------
+# If your want to other，please change this. | 想刷啥就改啥
 colo = "HKG"
-# Please set your warp path.
+# Set the overtime time | 设置超时时间
+overtime = 5
+# Please set your warp path. | 填入官方WARP软件安装路径
 WARP_path = "C:\\Program Files\\Cloudflare\\Cloudflare WARP\\Cloudflare WARP.exe"
+# Set the Ping List | 设置Ping网站清单
+web_list=["google","baidu","bilibili"] #Only www.___.com -> www.<web_list>.com | 仅支持www.___.com的形式
+#
 
 
 # Get UAC
@@ -40,8 +47,13 @@ while True:
     print("                WARP-PLUS-HKG (script)" + " By Wind_la")
     print("")
     x = x + 1
-    print(f"[!] 第{x}次连接 1.1.1.1")
-    trace = requests.get("https://1.1.1.1/cdn-cgi/trace").text
+    print(f"[!] 第{x}次连接 1.1.1.1 trace")
+    # trace = str("")
+    try:
+        trace = requests.get("https://1.1.1.1/cdn-cgi/trace", timeout=overtime).text
+    except requests.exceptions.RequestException:    # 蚌埠住了
+        print("[-] Warning: Overtime!!!")
+        trace = str("") # 双保险(?)
     if trace.find(warp) == -1:
         print("[-] WARP+: no")
     else:
@@ -62,10 +74,17 @@ while True:
 
 # End
 print("[!]-------------------- 连接", colo,"成功! --------------------")
-ping_google = int(ping('www.google.com')*1000)
-ping_baidu  = int(ping("www.baidu.com")*1000)
-print("[*] Ping Google: ", ping_google, "ms")
-print("[*] Ping  Baidu: ", ping_baidu, "ms")
+
+def pingtest(web): #Only www.XXX.com -> www.web.com
+    try:
+        web_ping = int(ping('www.'+str(web)+'.com')*1000)
+        return web_ping
+    except TypeError:
+        return "overtime"
+
+for web in web_list:
+    web_ping = pingtest(web)
+    print(f"[*] Ping {web}: ", web_ping, "ms")
 print("[!]--------------------------------------------------------")
 print("[!] 3s后窗口将自动关闭")
 time.sleep(3)
